030c0cbd42cbcc00a8e4108fc593f343e5a80ab6
DStanarevic <DejanStanarevic@gmail.com>
2017-10-29 21:42:47 -0400

    Update

---
+++ MVC_Review_Project/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Project.Models
+{
+    public class Category
+    {
+        //This is my category model, this includes a city along with a brief description.
+        [Key]
+        public int ID { get; set; }
+        [Display(Name ="City")]
+        public string Place { get; set; }
+        public string Description { get; set; }
+
+        //This links to my foreign key in the reviews table.
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ MVC_Review_Project/Models/Review.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Project.Models
+{
+    public class Review
+    {
+        //This is my reviews table, here I included a brief description along with a review score.
+        [Key]
+        public int ID { get; set; }
+
+        // Name of the site you would like to visit.
+        [Display(Name ="Location")]
+        public string Place { get; set; } 
+
+        //What kind of location is it, this can be a museum or a park or anything of that sort.
+        [Display(Name ="Type of location")]
+        public string Type { get; set; }
+
+        //Where exactly is it located in the city.
+        [Display(Name ="Where is it?")]
+        public string Location { get; set; }
+
+        //Brief description of the site and review.
+        [Display(Name ="Description")]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        //The review score for the site.
+        [Display(Name ="Fun Score")]
+        [Range(0, 5)]
+        public double FunLevel { get; set; }
+
+        //Start Time
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]
+        public DateTime From { get; set; }
+
+        //End Time
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]
+        public DateTime Until { get; set; }
+
+        //This brings over the Category ID and sets it as my foreign key.
+        [ForeignKey("Category"), Display(Name = "City")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
